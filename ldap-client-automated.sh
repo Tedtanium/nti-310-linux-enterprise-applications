7fb92c5c912c2a6e12acfbc3201616f888efe05f
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-04-18 19:57:08 -0700

    Create ldap-client-automated.sh

---
+++ ldap-client-automated.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get debconf-utils -y
+wget https://raw.githubusercontent.com/Tedtanium/nti-310-linux-enterprise-applications/master/ldap_debconf
+
+while read line; do echo "$line" | debconf-set-selections; done < ldap_debconf
+
+apt-get install -y libpam-ldap nscd
+
+sed -i 's/compat/compat ldap/g' /etc/nsswitch.conf
+
+/etc/init.d/nscd restart
+
+getent passwd
+
+#Just in case.
+#sed -i 's,uri ldapi:///,uri ldap://10.142.0.2,g' /etc/ldap.conf
+#sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
+#To test: Go into the client and use [getent passwd | thetrashman].
