00e80f4c3eb102dcb8177db09d21869042db17ef
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-04-25 10:27:42 -0700

    Create nfs+ldap-client-automated.sh

---
+++ nfs+ldap-client-automated.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get debconf-utils -y
+wget https://raw.githubusercontent.com/Tedtanium/nti-310-linux-enterprise-applications/master/ldap_debconf
+
+while read line; do echo "$line" | debconf-set-selections; done < ldap_debconf
+
+apt-get install -y libpam-ldap nscd
+
+sed -i 's/compat/compat ldap/g' /etc/nsswitch.conf
+
+/etc/init.d/nscd restart
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication Yes/g' /etc/ssh/sshd_config
+
+/etc/init.d/ssh restart
+
+export DEBIAN_FRONTEND=interactive
+
+#Just in case.
+#sed -i 's,uri ldapi:///,uri ldap://10.142.0.2,g' /etc/ldap.conf
+#sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
+#To test: Go into the client and use [getent passwd | grep thetrashman].
+
+#NFS client starts here.
+
+apt-get install nfs-client -y
+mkdir /mnt/test 
+echo "10.128.0.8:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
+
+#To be continued...
