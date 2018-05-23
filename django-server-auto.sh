ab5050fa9c1a9e0123d096507a65b3e3d20a9241
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-05-22 18:21:55 -0700

    Update django-server-auto.sh

--- django-server-auto.sh
+++ django-server-auto.sh
@@ -15,5 +15,10 @@ django-admin.py startproject myproject /opt/myproject
 #sed -i "s|'HOST': ''|'HOST': '${djangoip}'|g" /opt/myproject/myproject/settings.py
 #sed -i 's/'\''PORT'\'': '\'''\''/'\''PORT'\'': '\''5432'\''/g' /opt/myproject/myproject/settings.py
 
+mv /opt/myproject/myproject/settings.py /opt/myproject/myproject/settings.py.bak
+wget https://raw.githubusercontent.com/Tedtanium/nti-310-linux-enterprise-applications/master/django-thing/settings.py --directory-prefix=/opt/myproject/myproject/settings.py
+sed -i "s|'HOST': '1.2.3.4'|'HOST': '${djangoip}'|g" /opt/myproject/myproject/settings.py
+#How is $djangoip transferred from the master server to here?
+
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | ./manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | /opt/myproject/myproject/manage.py shell
