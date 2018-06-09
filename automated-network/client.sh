0345a92b6be52ac30c904fade72efbc760a62d17
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-06-08 19:23:16 -0700

    Update client.sh

--- automated-network/client.sh
+++ automated-network/client.sh
@@ -31,7 +31,7 @@ export DEBIAN_FRONTEND=interactive
 
 sed -i 's,uri ldapi:///,uri ldap://LDAPIP,g' /etc/ldap.conf
 sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
-#To test: Go into the client and use [getent passwd | grep thetrashman].
+#To test: Go into the client and use [getent passwd | grep 500].
 
 #NFS client starts here.
 
