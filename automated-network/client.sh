4142c8c7d862658bbaa13c69ec045b5d6387bab3
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-06-10 09:43:11 -0700

    Update client.sh

--- automated-network/client.sh
+++ automated-network/client.sh
@@ -7,7 +7,7 @@ apt-get --yes install libpam-ldap nscd
 echo -e "ldap-auth-config        ldap-auth-config/rootbindpw     password
 ldap-auth-config        ldap-auth-config/bindpw password
 ldap-auth-config        ldap-auth-config/override       boolean true
-ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=nti310,dc=local
+ldap-auth-config        ldap-auth-config/ldapns/base-dn string  dc=capstone,dc=local
 ldap-auth-config        ldap-auth-config/ldapns/ldap_version    select  3
 ldap-auth-config        ldap-auth-config/binddn string  cn=proxyuser,dc=example,dc=net
 ldap-auth-config        ldap-auth-config/dbrootlogin    boolean false
@@ -30,7 +30,7 @@ sed -i 's/PasswordAuthentication no/PasswordAuthentication Yes/g' /etc/ssh/sshd_
 export DEBIAN_FRONTEND=interactive
 
 sed -i 's,uri ldapi:///,uri ldap://LDAPIP,g' /etc/ldap.conf
-sed -i 's/base dc=example,dc=net/base dc=nti310,dc=local/g' /etc/ldap.conf
+sed -i 's/base dc=example,dc=net/base dc=capstone,dc=local/g' /etc/ldap.conf
 #To test: Go into the client and use [getent passwd | grep 500].
 
 #NFS client starts here.
